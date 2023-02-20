24526e3c3d7595f2aac77c4b6a4f6109a36f8b08
Anton <antosha.uvarov.90@mail.ru>
2023-02-21 00:58:54 +0300

    posts:done

--- client/src/components/Pages/Posts.jsx
+++ client/src/components/Pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MyPostForm from '../PostForm/MyPostForm';
 import '../../styles/App.css';
 import PostFilter from '../PostFilter/PostFilter';
@@ -21,18 +21,37 @@ function Posts() {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+
   // кастомный хук который содержит логику сортировки и фильтрации
   const sortedAndSerchedPosts = usePosts(posts, filter.sort, filter.query);
+  const lastElement = useRef();
+  const observer = useRef();
+  // console.log(lastElement);
   const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPagesCount(totalCount, limit));
   });
+
+  useEffect(() => {
+    if (isPostLoading) return;
+    if (observer.current) observer.current.disconnect();
+    const callback = function (entries, observer) {
+      if (entries[0].isIntersecting && page < totalPages) {
+        setPage(page + 1);
+      }
+      /* content exerted, snow below */
+      console.log(entries);
+    };
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+  }, [isPostLoading]);
+
   // console.log('page', page);
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page]);
 
   const createPost = (newPost) => { // добавление постов
     setPosts([...posts, newPost]);
@@ -46,7 +65,6 @@ function Posts() {
   const changePage = (p) => {
     // console.log(p);
     setPage(p);
-    fetchPosts(limit, p);
   };
 
   return (
@@ -69,13 +87,10 @@ function Posts() {
   {postError}
 </h1>
 )}
+      <PostList remove={removePost} posts={sortedAndSerchedPosts} title="Posts JS" />
+      <div ref={lastElement} style={{ height: 20, background: 'red' }} />
       {isPostLoading
-        ? (
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-            <Loader />
-          </div>
-        )
-        : <PostList remove={removePost} posts={sortedAndSerchedPosts} title="Posts JS" />}
+        && <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>}
       <Pagination page={page} changePage={changePage} totalPages={totalPages} />
     </div>
   );
